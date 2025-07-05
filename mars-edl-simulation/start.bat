c6052df6553ad4a890a217cac44c334902500dda
Sonica <shreya.boyane@gmail.com>
2025-07-05 16:46:47 -0400

    feat: add start script for Mars EDL simulation

---
+++ mars-edl-simulation/start.bat
@@ -0,0 +1,26 @@
+@echo off
+echo Starting Mars 2020 EDL Simulation...
+echo.
+
+echo Installing dependencies...
+npm install
+
+echo.
+echo Starting development servers...
+echo Server will run on http://localhost:3001
+echo Client will run on http://localhost:3000
+echo.
+
+start cmd /k "npm run server"
+timeout /t 3
+start cmd /k "npm run client"
+
+echo.
+echo Opening browser...
+timeout /t 5
+start http://localhost:3000
+
+echo.
+echo Mars 2020 EDL Simulation is starting up!
+echo Press any key to exit this launcher...
+pause > nul 
