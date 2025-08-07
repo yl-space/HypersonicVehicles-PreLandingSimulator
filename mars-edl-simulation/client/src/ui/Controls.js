275de2aab3a08833547e3633309275e16d1c1cd7
Sonica <shreya.boyane@gmail.com>
2025-08-06 20:37:39 -0400

    feat: update Controls UI component in client module

--- mars-edl-simulation/client/src/ui/Controls.js
+++ mars-edl-simulation/client/src/ui/Controls.js
@@ -38,11 +38,11 @@ export class Controls {
                     </svg>
                     FOLLOW
                 </button>
-                <button class="camera-mode" data-mode="FREE">
+                <button class="camera-mode" data-mode="FREESTYLE">
                     <svg width="20" height="20" viewBox="0 0 24 24">
                         <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-14c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6zm0 10c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4z" fill="currentColor"/>
                     </svg>
-                    FREE
+                    FREESTYLE
                 </button>
                 <button class="camera-mode" data-mode="CINEMATIC">
                     <svg width="20" height="20" viewBox="0 0 24 24">
