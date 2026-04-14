2f824ae74c902cd4e8911878cdac53d4b4fe4b58
opadun1 <opadun@wpi.edu>
2026-04-14 11:34:48 -0400

    Update Default Initial Conditions to be consistent with MSL

--- sim-server/src/sim_server/constants/defaults.py
+++ sim-server/src/sim_server/constants/defaults.py
@@ -6,12 +6,12 @@ DEFAULT_PLANET = {
 }
 
 DEFAULT_INIT = {
-    "h0": 124999, # [m] Critical altitude (i.e. altitude to start entry) [m] ref - 125e3 - Li ,Jiang 2014  MSL; Note- Girija 2022 is 120e3. I made I lower for the dataset
-    "vel0": 6.0836e3, # [m/s] MSL SPICE data
-    "theta0": np.deg2rad(-78.8618), #Initial longitude of probe [rad] ref: SPICE J2000 MSL initial position
-    "phi0": np.deg2rad(27.1050), #Initial latitude of probe [rad] ref: SPICE J2000 MSL initial position
-    "gamma0": np.deg2rad(-15.5), #flight path angle [rad] (should be negative)  ref - Li ,Jiang 2014  MSL
-    "psi0": np.deg2rad(0), #Initial heading angle [rad]
+    "h0": 125964.3, # [m] Critical altitude (i.e. altitude to start entry) [m] ref MSL SPICE data
+    "vel0": 5844.5850, # [m/s] MSL SPICE data
+    "theta0": np.deg2rad(126.7413), #Initial longitude of probe [rad] ref: SPICE J2000 MSL initial position
+    "phi0": np.deg2rad(-3.9222), #Initial latitude of probe [rad] ref: SPICE J2000 MSL initial position
+    "gamma0": np.deg2rad(-16.1304), #flight path angle [rad] (should be negative)  ref - Li ,Jiang 2014  MSL
+    "psi0": np.deg2rad(-3.3454), #Initial heading angle [rad]
 }
 
 DEFAULT_VEHICLE = {
@@ -19,7 +19,7 @@ DEFAULT_VEHICLE = {
 }
 
 DEFAULT_CONTROL = {
-    "bank_angle": np.deg2rad(30.0), # [rad] Bank Angle 
+    "bank_angle": np.deg2rad(0), # [rad] Bank Angle 
 }
 def override_defaults(defaults: dict, overrides: dict) -> dict:
     """Override default parameters with user-specified values, ignoring None values in overrides."""
